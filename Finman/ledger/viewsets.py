6672834741688055155189d298b680638d7312b1
Priyansh Bordia <62763160+PriyanshBordia@users.noreply.github.com>
2024-07-23 23:55:38 +0530

    update views

---
+++ Finman/ledger/viewsets.py
@@ -0,0 +1,23 @@
+from rest_framework import viewsets
+
+from .models import *
+from .serializers import *
+
+class EarningModelViewSet(viewsets.ModelViewSet):
+	queryset = Earning.objects.all().order_by('id')
+	serializer_class = EarningSerializer
+
+
+class InvestmentModelViewSet(viewsets.ModelViewSet):
+	queryset = Investment.objects.all()
+	serializer_class = InvestmentSerializer
+
+
+class PurchaseModelViewSet(viewsets.ModelViewSet):
+	queryset = Purchase.objects.all()
+	serializer_class = PurchaseSerializer
+
+
+class TaxModelViewSet(viewsets.ModelViewSet):
+	queryset = Tax.objects.all()
+	serializer_class = TaxSerializer
